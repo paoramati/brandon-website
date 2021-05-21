398672bef7ece17de398a030c2ab29f491694ad9
paoramati <brandonmitchellpaul@gmail.com>
2021-05-21 21:55:18 +1200

    Personalise & put under construction label

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Brandon Paul'
+export const siteTitle = 'Brandon Paul - Software Developer'
 
 export default function Layout({ children, home }) {
   return (
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/brandon.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
